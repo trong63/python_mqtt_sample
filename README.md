425a46a2f0b9cb287088206b4ccd48e63115678e
btran <btran@multitech.com>
2016-01-19 13:07:10 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# python_mqtt_sample
+#python_mqtt_sample
+
+This is a python mqtt client sample code to allow to subscribe to the Conduit's MQTT server and echoing/send (publish) the data back to mDot.
+
+Installation:
+
+1. ssh into the Conduit and type the following command to install pip. Make sure your Conduit is connected to the internet.
+   
+   opkg update
+
+   opkg install python-pip
+
+   wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py --no-check-certificate
+
+   python ez_setup.py
+
+2. Type: pip install paho-mqtt
+
+3. scp the mqtt-client.py to your Conduit
+
+4. Run it: python mqtt-client.py
+
+ 
---
+++ mqtt-client.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#BT - On the Conduit, type the following commands
+#pip install paho-mqtt
+
+import paho.mqtt.client as mqtt
+import json
+
+
+
+def on_connect(client, userdata, rc):
+
+	print("Connected with result code "+str(rc))
+	client.subscribe("lora/+/up")
+
+
+def on_message(client, userdata, msg):
+	print("Topic: ", msg.topic+'\nMessage: '+str(msg.payload))
+	#BT - Extract out the mac address in the topic
+	mac_node = msg.topic.split('/')
+	print("BT - mac_node: " + mac_node[1])
+	#BT - Convert to json format data structure
+	json_data = json.loads(msg.payload.decode("utf-8"))
+	topic_down = "lora/" + mac_node[1] + "/down"
+        print("BT - Topic down: " + topic_down)
+	print("BT - json_data: %s" %(json_data["data"]))
+	data_to_mDot = "{ \"data\": \"" + json_data["data"] + "\" }"
+        print("BT - data_to_mDot: " + data_to_mDot)
+	client.publish(topic_down,data_to_mDot)
+
+
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("127.0.0.1", 1883, 60)
+
+
+client.loop_forever()
+
+
